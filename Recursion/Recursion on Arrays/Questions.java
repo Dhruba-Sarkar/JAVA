715520db245db917557cee9077c66c851e2a24b3
Dhruba Sarkar <dhruba.sarkar.pro@gmail.com>
2026-08-12 15:31:18 +0530

    add more questions

--- Recursion/Recursion on Arrays/Questions.java
+++ Recursion/Recursion on Arrays/Questions.java
@@ -79,6 +79,7 @@ public class Questions {
         return ans;
     }
 
+    // Get Stair Paths
     public static ArrayList<String> getStairPaths(int n) {
 
         ArrayList<String> allPaths = new ArrayList<>();
@@ -113,6 +114,101 @@ public class Questions {
         return allPaths;
     }
 
+    // Find the First Index of an Element in an Array
+    public static int findFirstIndex(int[] arr, int tar, int idx) {
+
+        if (idx == arr.length) {
+            return -1;
+        }
+
+        int furtherFirstIndex = findFirstIndex(arr, tar, idx + 1);
+        if (arr[idx] == tar) {
+            return idx;
+        } else {
+            return furtherFirstIndex;
+        }
+    }
+
+    // Find the Last Index of an Element in an Array
+    public static int findLastIndex(int[] arr, int tar, int idx) {
+        if (idx == arr.length) {
+            return -1;
+        }
+
+        int lastIndex = findLastIndex(arr, tar, idx + 1);
+
+        if (lastIndex == -1 && arr[idx] == tar) {
+            return idx;
+        }
+        return lastIndex;
+    }
+
+    // Find All Indices of an Element in an Array
+    public static int[] findAllIndices(int[] arr, int tar, int idx, int fsf) {
+        if (idx == arr.length) {
+            int baseArr[] = new int[fsf];
+            return baseArr;
+        }
+        int[] ans;
+        if (arr[idx] == tar) {
+            ans = findAllIndices(arr, tar, idx + 1, fsf + 1);
+            ans[fsf] = idx;
+        } else {
+            ans = findAllIndices(arr, tar, idx + 1, fsf);
+        }
+        return ans;
+    }
+
+    // Get All Maze Paths
+    public static ArrayList<String> getMazePaths(int sr, int sc, int dr, int dc) {
+        ArrayList<String> allPaths = new ArrayList<>();
+        if (sr > dr || sc > dc) {
+            return allPaths;
+        }
+        if (sr == dr && sc == dc) {
+            allPaths.add("");
+            return allPaths;
+        }
+
+        ArrayList<String> pathsAfterHStep = getMazePaths(sr, sc + 1, dr, dc);
+        ArrayList<String> pathsAfterVStep = getMazePaths(sr + 1, sc, dr, dc);
+
+        for (String path : pathsAfterHStep) {
+            allPaths.add("h" + path);
+        }
+        for (String path : pathsAfterVStep) {
+            allPaths.add("v" + path);
+        }
+        return allPaths;
+    }
+
+    // Get Maze Paths With Jumps
+    public static ArrayList<String> getMazePathWithJumps(int sr, int sc, int dr, int dc) {
+        ArrayList<String> allPaths = new ArrayList<>();
+        if (sr == dr && sc == dc) {
+            allPaths.add("");
+            return allPaths;
+        }
+        // Horizontal Jumps
+        for (int jump = 1; jump <= dc - sc; jump++) {
+            ArrayList<String> pathsAfterHJump = getMazePathWithJumps(sr, sc + jump, dr, dc);
+
+            for (String path : pathsAfterHJump) {
+                allPaths.add("h" + jump + path);
+            }
+        }
+
+        // Vertical Jumps
+        for (int jump = 1; jump <= dr - sr; jump++) {
+            ArrayList<String> pathsAfterVJump = getMazePathWithJumps(sr + jump, sc, dr, dc);
+
+            for (String path : pathsAfterVJump) {
+                allPaths.add("v" + jump + path);
+            }
+        }
+        return allPaths;
+    }
+
     public static void main(String[] args) {
         // int arr[] = { 1, 7, 45, 8, 11 };
         // printArray(arr, 0);
@@ -120,6 +216,17 @@ public class Questions {
         // System.out.println(findMax(arr, 0));
         // System.out.println(getSubsequences("abcd"));
         // System.out.println(getKpc("789"));
-        System.out.println(getStairPaths(4));
+        // System.out.println(getStairPaths(4));
+        // int arr[] = { 10, 19, 4, 5, 1, 19, 4, 4, 11, 19 };
+        // System.out.println(findFirstIndex(arr, 4, 0));
+        // System.out.println(findLastIndex(arr, 19, 0));
+
+        // int ans[] = findAllIndices(arr, 4, 0, 0);
+        // for (int i = 0; i < ans.length; i++) {
+        // System.out.print(ans[i] + " ");
+        // }
+
+        ArrayList<String> ans = getMazePathWithJumps(0, 0, 2, 2);
+        System.out.println(ans);
     }
 }
