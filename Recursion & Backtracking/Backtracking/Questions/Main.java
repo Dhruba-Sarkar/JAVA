248c97e1b6b6c403b1fe326576bbb62996a4395e
Dhruba Sarkar <dhruba.sarkar.pro@gmail.com>
2026-08-28 11:38:25 +0530

    add more question

--- Recursion & Backtracking/Backtracking/Questions/Main.java
+++ Recursion & Backtracking/Backtracking/Questions/Main.java
@@ -11,10 +11,103 @@ public class Main {
         printAllSubset(nums, idx + 1, targetSum, asf); // No Call
     }
 
-    public static void main(String[] args) {
-        int nums[] = { 2, 2, 3, 11, 1, 4, 0 };
-        int targetSum = 5;
+    // Lets play Chess
+    // N Queens
+    public static void printVisitedMatrix(boolean[][] vis) {
+        for (int i = 0; i < vis.length; i++) {
+            for (int j = 0; j < vis[0].length; j++) {
+                if (vis[i][j] == true) {
+                    System.out.print("( " + i + "," + j + " ) ");
+                }
+            }
+        }
+        System.out.println();
+    }
 
-        printAllSubset(nums, 0, targetSum, "");
+    public static boolean isPossibleToPlaceHere(int row, int col, boolean[][] vis) {
+        int n = vis.length;
+        int[][] dirs = { { -1, -1 }, { -1, 0 }, { -1, 1 }, { 0, 1 }, { 1, 1 }, { 1, 0 }, { 1, -1 }, { 0, -1 } };
+        // { -1, -1 }, { -1, 0 }, { -1, 1 } only these directions are required
+
+        for (int rad = 1; rad < n; rad++) {
+            for (int[] dir : dirs) {
+                int nRow = row + rad * dir[0];
+                int nCol = col + rad * dir[1];
+
+                if (nRow >= 0 && nCol >= 0 && nRow < n && nCol < n && vis[nRow][nCol] == true) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public static void nQueens(int row, int n, boolean[][] vis) {
+        if (row == n) {
+            printVisitedMatrix(vis);
+            return;
+        }
+
+        for (int col = 0; col < n; col++) {
+            if (isPossibleToPlaceHere(row, col, vis) == true) {
+                vis[row][col] = true;
+                nQueens(row + 1, n, vis);
+                vis[row][col] = false;
+            }
+        }
+    }
+
+    // Knight Tour
+    public static void printKnightTour(int sr, int sc) {
+        int[][] board = new int[8][8];
+
+        knightTour(sr, sc, 0, board);
+    }
+
+    public static void knightTour(int row, int col, int moveNumber, int[][] board) {
+        if (row < 0 || col < 0 || row >= board.length || col >= board[0].length || board[row][col] != 0) {
+            return;
+        }
+        if (moveNumber == board.length * board.length) { // moveNumber == 64
+            printChessBoard(board);
+            return;
+        }
+
+        board[row][col] = moveNumber;
+
+        knightTour(row - 2, col - 1, moveNumber + 1, board);
+        knightTour(row - 2, col + 1, moveNumber + 1, board);
+        knightTour(row - 1, col + 2, moveNumber + 1, board);
+        knightTour(row + 1, col + 2, moveNumber + 1, board);
+        knightTour(row + 2, col + 1, moveNumber + 1, board);
+        knightTour(row + 2, col - 1, moveNumber + 1, board);
+        knightTour(row + 1, col - 2, moveNumber + 1, board);
+        knightTour(row - 1, col - 2, moveNumber + 1, board);
+
+        board[row][col] = 0;
+    }
+
+    public static void printChessBoard(int[][] board) {
+        System.out.println("Printing a solution ========");
+
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        // int nums[] = { 2, 2, 3, 11, 1, 4, 0 };
+        // int targetSum = 5;
+
+        // printAllSubset(nums, 0, targetSum, "");
+
+        // int n = 4;
+        // boolean[][] vis = new boolean[n][n];
+        // nQueens(0, n, vis);
+
+        printKnightTour(3, 4);
     }
 }
--- Recursion & Backtracking/Recursion/Misc Problems/Main.java
+++ Recursion & Backtracking/Recursion/Misc Problems/Main.java
@@ -1,5 +1,7 @@
 
 public class Main {
+
+    // Find Max Path Sum
     public static int findMaxSum(int[][] grid, int row, int col, int n, int m) {
 
         if (row >= n || col >= m) {
@@ -21,6 +23,19 @@ public class Main {
 
     public static int maxSum(int[][] grid) {
         return findMaxSum(grid, 0, 0, grid.length, grid[0].length);
+    }
+
+    // Permutaion of String
+    public static void permutaitionOfString(String str, String asf) {
+
+        if (str.length() == 0) {
+            System.out.println(asf);
+            return;
+        }
+
+        char firstChar = str.charAt(0);
+        String sm = str.substring(1);
+        permutaitionOfString(str, asf + firstChar);
     }
 
     public static void main(String[] args) {
