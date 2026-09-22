119b6c0aedeb658c13da1074fec307de234b9f67
Dhruba Sarkar <dhruba.sarkar.pro@gmail.com>
2026-09-22 16:42:49 +0530

    More LinkList Questions

--- LinkedList/Questions/Main.java
+++ LinkedList/Questions/Main.java
@@ -274,6 +274,109 @@ public class Main {
         return dummy.next;
     }
 
+    // Leetcode 141 -> Linked List Cycle
+    public boolean hasCycle(ListNode head) {
+        if (head == null || head.next == null) {
+            return false;
+        }
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (slow == fast) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // Leetcode 142 -> Linked List Cycle II
+    public ListNode detectCycle(ListNode head) {
+        if (head == null || head.next == null) {
+            return null;
+        }
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (slow == fast) {
+                break;
+            }
+        }
+        if (slow != fast) { // No cycle exist
+            return null;
+        }
+
+        // Find starting point
+        slow = head;
+        while (slow != fast) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        return fast;
+    }
+
+    // Leetcode 237 -> Delete Node in a Linked List
+    public void deleteNode(ListNode node) {
+        ListNode nodeKaNext = node.next;
+
+        // delete the next node
+        node.next = node.next.next;
+
+        // copy nodeKa data
+        node.val = nodeKaNext.val;
+    }
+
+    // Leetcode 160 -> Intersection of Two Linked Lists
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode temp = headA;
+
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+        ListNode tailA = temp;
+
+        tailA.next = headA; // or headB
+        ListNode interSectionNode = detectCycle(headB); // if tail is connected to headB, send headA here
+
+        tailA.next = null; // fixing the list again
+
+        return interSectionNode;
+    }
+
+    // Leetcode 21 -> Merge Two Sorted Lists
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode temp1 = list1;
+        ListNode temp2 = list2;
+
+        ListNode dummy = new ListNode(-1);
+        ListNode curr = dummy;
+
+        while (temp1 != null && temp2 != null) {
+            if (temp1.val < temp2.val) {
+                curr.next = temp1;
+                temp1 = temp1.next;
+            } else {
+                curr.next = temp2;
+                temp2 = temp2.next;
+            }
+            curr = curr.next;
+        }
+        // connect rest of the list
+        if (temp1 == null) {
+            curr.next = temp2;
+        } else {
+            curr.next = temp1;
+        }
+        return dummy.next;
+    }
+
     public static void main(String[] args) {
         //
     }
